33f372dc999818e197bcc1905433c8fac23a6b55
ryouichi-kurihara <ryouichi.kurihara.628@gmail.com>
2022-07-10 14:23:00 +0900

    add listView and detailView

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,8 @@
-from django.contrib import admin
 from django.urls import path, include
+from .views import TodoList
+from .views import TodoDetail
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('list/', TodoList.as_view()),
+    path('detail/<int:pk>', TodoDetail.as_view())
 ]
